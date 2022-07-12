155acfe7434453bf1738fce1b59716473a589de4
John Zinno <62895131+jzinno@users.noreply.github.com>
2022-07-12 17:14:47 -0400

    Formatting and linting

--- scripts/drain-the-box.py
+++ scripts/drain-the-box.py
@@ -1,13 +1,9 @@
 import argparse
-import os
-import re
 
 # I/O
-parser = argparse.ArgumentParser(description = "Remove solvent from a pdb file")
-parser.add_argument("-i", "--input", dest = "input", required = True,
-                    help = "Input pdb file")
-parser.add_argument("-o", "--output", dest = "output", required = True,
-                    help = "Output file")
+parser = argparse.ArgumentParser(description="Remove solvent from a pdb file")
+parser.add_argument("-i", "--input", dest="input", required=True, help="Input pdb file")
+parser.add_argument("-o", "--output", dest="output", required=True, help="Output file")
 args = parser.parse_args()
 
 # Arguments
@@ -17,18 +13,20 @@ outputfile = args.output
 # Initialize variables
 ATOM_list = []
 
-def read_pdb (pdbfile):
+
+def read_pdb(pdbfile):
     """
     Read through a pdb file and save the essential protein model lines
     """
     with open(pdbfile, "r") as pdb:
         for line in pdb:
-            if line.startswith(('ATOM','TER','MODEL','ENDMDL','END')):
+            if line.startswith(("ATOM", "TER", "MODEL", "ENDMDL", "END")):
                 ATOM_list.append(line)
     pdb.close()
     return ATOM_list
 
-def write_pdb (outputfile):
+
+def write_pdb(outputfile):
     """
     Write out a pdb file with only the essential protein model lines
     """
@@ -36,6 +34,7 @@ def write_pdb (outputfile):
         for line in ATOM_list:
             out.write(line)
     out.close()
+
 
 ATOM_list = read_pdb(inputfile)
 write_pdb(outputfile)
