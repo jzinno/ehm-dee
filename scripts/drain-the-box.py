e8da0058de04148d24d57d9680d5fca0b3b338d2
John Zinno <62895131+jzinno@users.noreply.github.com>
2022-07-12 12:53:19 -0400

    Initial upload

---
+++ scripts/drain-the-box.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import re
+
+# I/O
+parser = argparse.ArgumentParser(description = "Remove solvent from a pdb file")
+parser.add_argument("-i", "--input", dest = "input", required = True,
+                    help = "Input pdb file")
+parser.add_argument("-o", "--output", dest = "output", required = True,
+                    help = "Output file")
+args = parser.parse_args()
+
+# Arguments
+inputfile = args.input
+outputfile = args.output
+
+# Initialize variables
+ATOM_list = []
+
+def read_pdb (pdbfile):
+    """
+    Read through a pdb file and save the essential protein model lines
+    """
+    with open(pdbfile, "r") as pdb:
+        for line in pdb:
+            if line.startswith(('ATOM','TER','MODEL','ENDMDL','END')):
+                ATOM_list.append(line)
+    pdb.close()
+    return ATOM_list
+
+def write_pdb (outputfile):
+    """
+    Write out a pdb file with only the essential protein model lines
+    """
+    with open(outputfile, "w") as out:
+        for line in ATOM_list:
+            out.write(line)
+    out.close()
+
+ATOM_list = read_pdb(inputfile)
+write_pdb(outputfile)
