4d415ec1b147b6e20b912d72888e3446994b2379
John Zinno <johnzinno14@gmail.com>
2023-09-20 22:20:25 -0400

    refactor code

--- scripts/ehmdee.py
+++ scripts/ehmdee.py
@@ -9,163 +9,171 @@ from openmm.app import *
 from openmm import *
 from openmm.unit import *
 
-# I/O
-parser = argparse.ArgumentParser(description="Runs a Molecular Dynamics Simulation")
-parser.add_argument("-i", "--input", dest="input", required=True, help="Input pdb file")
-parser.add_argument(
-    "-o",
-    "--output_path",
-    dest="output_path",
-    type=str,
-    default="./output",
-    help="Path to save output files",
-)
-parser.add_argument(
-    "-gpu",
-    "--use_gpu",
-    dest="gpu",
-    type=bool,
-    default=True,
-    help="Enables running simulation on GPU",
-)
-parser.add_argument(
-    "-s",
-    "--solvent",
-    dest="solvent",
-    type=bool,
-    default=True,
-    help="Adds solvent to the simulation",
-)
-parser.add_argument(
-    "-m",
-    "--minimize",
-    dest="minimize",
-    type=bool,
-    default=True,
-    help="Performs Energy Minimization of the structure before simulation",
-)
-parser.add_argument(
-    "-t",
-    "--time_step",
-    dest="time_step",
-    type=int,
-    default=5000,
-    help="Number of time-steps to perform",
-)
-parser.add_argument(
-    "-r",
-    "--report_interval",
-    dest="report_interval",
-    type=int,
-    default=1000,
-    help="Frequency of simulation reports in time-steps",
-)
-args = parser.parse_args()
 
-# Arguments
-inputfile = args.input
-output_path = args.output_path
-gpu = args.gpu
-solvent = args.solvent
-minimize = args.minimize
-time_step = args.time_step
-report_interval = args.report_interval
+class MolecularDynamicsSimulation:
+    def __init__(self, args):
+        self.inputfile = args.input
+        self.output_path = args.output_path
+        self.gpu = args.gpu
+        self.solvent = args.solvent
+        self.minimize = args.minimize
+        self.time_step = args.time_step
+        self.report_interval = args.report_interval
+        self.sim_name = basename(self.inputfile).rsplit(".", 1)[0]
+        self.output_dir = os.path.join(self.output_path, self.sim_name)
+        os.makedirs(self.output_dir, exist_ok=True)
+        self.platform = (
+            Platform.getPlatformByName("CUDA")
+            if self.gpu
+            else Platform.getPlatformByName("CPU")
+        )
 
-# Setting up ID name
-sim_name = basename(inputfile).rsplit(".", 1)[0]
-print("Preparing simulation of: " + sim_name)
+    def load_input(self):
+        self.pdb = PDBFile(self.inputfile)
+        self.modeller = Modeller(self.pdb.topology, self.pdb.positions)
+        self.forcefield = ForceField("amber14-all.xml", "amber14/tip3pfb.xml")
+        self.modeller.addHydrogens(self.forcefield)
 
-# Setting up output directory
-output_dir = os.path.join(output_path, sim_name)
-os.makedirs(output_dir)
+    def add_solvent(self):
+        if self.solvent:
+            self.modeller.addSolvent(self.forcefield, padding=1.0 * nanometers)
+            with open(
+                os.path.join(self.output_dir, self.sim_name) + "_post-solvent.pdb", "w"
+            ) as postsolvent:
+                PDBFile.writeFile(
+                    self.modeller.topology, self.modeller.positions, postsolvent
+                )
 
-# Setting up platform
-if gpu is True:
-    platform = Platform.getPlatformByName("CUDA")
-else:
-    platform = Platform.getPlatformByName("CPU")
-print("Simulation platform: " + platform.getName())
+    def create_system(self):
+        self.system = self.forcefield.createSystem(
+            self.modeller.topology,
+            nonbondedMethod=NoCutoff,
+            nonbondedCutoff=1 * nanometer,
+            constraints=HBonds,
+        )
+        self.integrator = LangevinMiddleIntegrator(
+            293.15 * kelvin, 1 / picosecond, 0.004 * picoseconds
+        )
+        self.simulation = Simulation(
+            self.modeller.topology, self.system, self.integrator, self.platform
+        )
+        self.simulation.context.setPositions(self.modeller.positions)
 
-# Loading input
-pdb = PDBFile(inputfile)
-print("Input file loaded")
+    def energy_minimization(self):
+        if self.minimize:
+            self.simulation.minimizeEnergy()
+            state = self.simulation.context.getState(getEnergy=True)
+            with open(
+                os.path.join(self.output_dir, self.sim_name) + "_minimized.pdb", "w"
+            ) as minimized:
+                positions = self.simulation.context.getState(
+                    getPositions=True
+                ).getPositions()
+                PDBFile.writeFile(self.simulation.topology, positions, minimized)
 
-modeller = Modeller(pdb.topology, pdb.positions)
+    def add_reporters(self):
+        self.simulation.reporters.append(
+            PDBReporter(
+                os.path.join(self.output_dir, self.sim_name) + "_out.pdb",
+                self.report_interval,
+            )
+        )
+        self.simulation.reporters.append(
+            StateDataReporter(
+                sys.stdout,
+                self.report_interval,
+                step=True,
+                potentialEnergy=True,
+                temperature=True,
+                speed=True,
+                elapsedTime=True,
+                progress=True,
+                remainingTime=True,
+                totalSteps=self.time_step,
+            )
+        )
+        self.simulation.reporters.append(
+            StateDataReporter(
+                os.path.join(self.output_dir, self.sim_name) + "_log.csv",
+                self.report_interval,
+                step=True,
+                potentialEnergy=True,
+                temperature=True,
+                speed=True,
+                elapsedTime=True,
+                progress=True,
+                remainingTime=True,
+                totalSteps=self.time_step,
+            )
+        )
 
-forcefield = ForceField("amber14-all.xml", "amber14/tip3pfb.xml")
-
-if solvent is True:
-    print("Filling the box with water")
-    modeller.addSolvent(forcefield, padding=1.0 * nanometers)
-
-    print("Saving solvated structure")
-    with open(
-        os.path.join(output_dir, sim_name) + "_post-solvent.pdb", "w"
-    ) as postsolvent:
-        PDBFile.writeFile(modeller.topology, modeller.positions, postsolvent)
-
-system = forcefield.createSystem(
-    modeller.topology,
-    nonbondedMethod=NoCutoff,
-    nonbondedCutoff=1 * nanometer,
-    constraints=HBonds,
-)
-
-integrator = LangevinMiddleIntegrator(
-    293.15 * kelvin, 1 / picosecond, 0.004 * picoseconds
-)
-
-simulation = Simulation(modeller.topology, system, integrator, platform)
-
-simulation.context.setPositions(modeller.positions)
-
-if minimize is True:
-    print("Running Energy Minimization")
-    simulation.minimizeEnergy()
-
-    state = simulation.context.getState(getEnergy=True)
-
-    print("Minimization complete E: " + str(state.getPotentialEnergy()))
-
-    print("Saving minimized structure")
-    with open(os.path.join(output_dir, sim_name) + "_minimized.pdb", "w") as minimized:
-        positions = simulation.context.getState(getPositions=True).getPositions()
-        PDBFile.writeFile(simulation.topology, positions, minimized)
+    def run_simulation(self):
+        self.simulation.step(self.time_step)
 
 
-simulation.reporters.append(
-    PDBReporter(os.path.join(output_dir, sim_name) + "_out.pdb", report_interval)
-)
-
-simulation.reporters.append(
-    StateDataReporter(
-        sys.stdout,
-        report_interval,
-        step=True,
-        potentialEnergy=True,
-        temperature=True,
-        speed=True,
-        elapsedTime=True,
-        progress=True,
-        remainingTime=True,
-        totalSteps=time_step,
+def main():
+    parser = argparse.ArgumentParser(description="Runs a Molecular Dynamics Simulation")
+    parser.add_argument(
+        "-i", "--input", dest="input", required=True, help="Input pdb file"
     )
-)
-
-simulation.reporters.append(
-    StateDataReporter(
-        os.path.join(output_dir, sim_name) + "_log.csv",
-        report_interval,
-        step=True,
-        potentialEnergy=True,
-        temperature=True,
-        speed=True,
-        elapsedTime=True,
-        progress=True,
-        remainingTime=True,
-        totalSteps=time_step,
+    parser.add_argument(
+        "-o",
+        "--output_path",
+        dest="output_path",
+        type=str,
+        default="./output",
+        help="Path to save output files",
     )
-)
+    parser.add_argument(
+        "-gpu",
+        "--use_gpu",
+        dest="gpu",
+        type=bool,
+        default=True,
+        help="Enables running simulation on GPU",
+    )
+    parser.add_argument(
+        "-s",
+        "--solvent",
+        dest="solvent",
+        type=bool,
+        default=True,
+        help="Adds solvent to the simulation",
+    )
+    parser.add_argument(
+        "-m",
+        "--minimize",
+        dest="minimize",
+        type=bool,
+        default=True,
+        help="Performs Energy Minimization of the structure before simulation",
+    )
+    parser.add_argument(
+        "-t",
+        "--time_step",
+        dest="time_step",
+        type=int,
+        default=5000,
+        help="Number of time-steps to perform",
+    )
+    parser.add_argument(
+        "-r",
+        "--report_interval",
+        dest="report_interval",
+        type=int,
+        default=1000,
+        help="Frequency of simulation reports in time-steps",
+    )
+    args = parser.parse_args()
 
-print("Beginning Time Steps")
-simulation.step(time_step)
+    simulation = MolecularDynamicsSimulation(args)
+    simulation.load_input()
+    simulation.add_solvent()
+    simulation.create_system()
+    simulation.energy_minimization()
+    simulation.add_reporters()
+    simulation.run_simulation()
+
+
+if __name__ == "__main__":
+    main()
